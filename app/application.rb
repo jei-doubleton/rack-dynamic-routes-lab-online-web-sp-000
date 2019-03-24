3fe13178fb6ed28c448c23f1680c59df51bb0c6e
J. Singleton <jennifer.singleton@gmail.com>
2019-03-24 20:14:16 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,5 @@
 class Application
 
-  @@items = Item.all
-
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
@@ -10,7 +8,7 @@ class Application
 
       item_name = req.path.split("/items/").last
 
-      if Item.find_by_name
+      if Item.find_by_name(item_name)
         item = Item.find_by_name
         resp.write "#{item.price}"
       else
