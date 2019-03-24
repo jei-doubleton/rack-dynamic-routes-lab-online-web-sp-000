62203b2400993a593206840e488fccdbaca7b41f
J. Singleton <jennifer.singleton@gmail.com>
2019-03-24 19:57:47 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,14 +1,23 @@
 class Application
 
+  @@items = Item.all
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match("/items/")
+      
       item_name = req.path.split("/items/").last
-      item = @@items.find{|i| i.name == item_name}
 
-      resp.write "#{item.price}"
+      if @@items.find{|i| i.name == item_name}
+        item = @@items.find{|i| i.name == item_name}
+        resp.write "#{item.price}"
+      else
+        resp.write "Sorry we don't have that item"
+        resp.status = 400
+      end
+      
     else
       resp.write "Error message"
       resp.status = 404
